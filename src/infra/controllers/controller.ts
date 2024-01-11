a63276100eea04d8670ee5e01a4cbf3537e4b61f
FabioKido <fabiohenryquemesquita@gmail.com>
2024-01-11 17:28:59 -0300

    feat(controller): created test controller

---
+++ src/infra/controllers/controller.ts
@@ -0,0 +1,20 @@
+import fs from 'fs'
+import { Request, Response } from 'express'
+import { BadRequestError } from '../helpers/errors'
+
+export const name = async(req: Request, res: Response) => {
+    const {name} = req.body
+
+    if(name.length < 4) throw new BadRequestError('Nome menor que o esperado...')
+    console.log(name)
+
+    res.end()
+}
+
+export const file = (req: Request, res: Response) => {
+    const file = fs.readFileSync('jest.config.js')
+
+    if(!file) throw new BadRequestError('Arquivo não encontrado...')
+
+    return res.json(file.toString())
+}
